024687746de46b1f46daf3f6f466bd034996f7e4
Bazhena <johurn@yandex.ru>
2016-10-08 15:55:35 +0300

    Исправление замечаний

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,7 @@ module.exports = function (grunt) {
           expand: true,
           src: [
             "fonts/**/*.{woff,woff2}",
+            "!**/icons/**",
             "img/**",
             "js/**",
             "*.html"
